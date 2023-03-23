4e29d826f9015a5002694695b3e4451cab66abf4
Nicholas Iten <iten.pn@gmail.com>
2023-03-23 10:57:17 -0400

    minor changes

--- index.js
+++ index.js
@@ -1,32 +1,35 @@
 /* Handles opening up a file  */
 function handleFile(input) {
-	const file = input.target.files[0];
-	const reader = new FileReader();
-	
-	reader.onload = (e) => {
-		const file = e.target.result;
-		const lines = file.split(/\r\n|\n/);
-		//TODO: Do something with the file
-		console.log(parseRriscFile(lines));
-	}
+  const file = input.target.files[0];
+  const reader = new FileReader();
 
-	reader.onerror = (e) => {
-		console.error(e.target.error.name);
-	}
+  reader.onload = (e) => {
+    const file = e.target.result;
+    const lines = file.split(/\r\n|\n/);
+    //TODO: Do something with the file
+    console.log(tokenizeRriscFile(lines));
+  };
 
-	reader.readAsText(file);
+  reader.onerror = (e) => {
+    console.error(e.target.error.name);
+  };
+
+  reader.readAsText(file);
 }
 
 /* Given an array of the lines of a RRISC file, generates an array where each entry
-	corresponds to each element of the command. */ 
-function parseRriscFile(lines) {
-	let cmds = [];
-	lines.forEach((line) => {
-		cmds.push( 
-			line.trim().split(/[ ,]/).filter((a)=>a.length>0)
-		);	
-	});	
-	return cmds.filter((a)=>a.length>0).filter((a)=>a[0][0]!==';');
+	corresponds to each element of the command. */
+function tokenizeRriscFile(lines) {
+  let cmds = [];
+  lines.forEach((line) => {
+    cmds.push(
+      line
+        .trim()
+        .split(/[ ,]/)
+        .filter((a) => a.length > 0)
+    );
+  });
+  return cmds.filter((a) => a.length > 0).filter((a) => a[0][0] !== ";");
 }
 
-document.getElementById('file').onchange = handleFile;
+document.getElementById("file").onchange = handleFile;
